b1f099c080d4d6dc9e773a8654760111c6a64496
Furkan <furi544@gmail.com>
2023-03-13 17:08:28 +0300

    travel test updated

--- 2-structuring-tests/src/travel.test.js
+++ 2-structuring-tests/src/travel.test.js
@@ -6,8 +6,24 @@ it("renders travel component", () => {
   const { container } = render(<Travel />);
   expect(container).toMatchInlineSnapshot(`
 <div>
-  <div>
-    snapshot
+  <div
+    class="card text-center m-1"
+    style="width: 18rem;"
+  >
+    <i
+      class="material-icons"
+      style="font-size: 4rem;"
+    >
+      airplanemode_active
+    </i>
+    <h4>
+      Travel Anywhere
+    </h4>
+    <p
+      class="p-1"
+    >
+      Our premium package allows you to take exotic trips anywhere at the cheapest prices!
+    </p>
   </div>
 </div>
 `);
